8ba2c75f2f06264f8e9fcc8f8f33fe982fe666b7
saurabhsigma <saurabh962003@gmail.com>
2024-09-07 12:15:16 +0530

    navbar added

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import View from "./View/page";
-import Write from "./Write/page"
+import Write from "./Write/page";
+import Nav from "@/components/navbar";
+import './globals.css';
 
 const page = () =>{
   return (
     <>
+    <div className="">
+      <Nav />
+    </div>
+    {/* <div>
       <h2>Welcome to the website!</h2>
       <Link href="/View">View your posts</Link>
       <br />
       <Link href="/Write">write new post</Link>
+    </div> */}
     </>
   )
 }
---
+++ frontend/components/navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import '../app/globals.css';
+import Link from "next/link";
+
+
+
+const Nav = () =>{
+
+    return(
+        <div className="flex flex-row bg-blue-400 font-bold justify-between p-3" >
+        
+            <div id="logo" className="">Logo</div>
+            <div id="links" className="">
+                <Link href="/View" className="m-2">View!</Link>
+                <Link href="/Write" className="m-2">Write!</Link>
+                <Link href="/Write" className="m-2">
+                    <button className="border-1 bg-white text-black p-1 rounded-md">Signup/Login!</button>
+                </Link>
+            </div>
+        
+        
+        </div>
+    )
+
+}
+
+export default Nav;
